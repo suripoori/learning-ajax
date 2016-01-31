1a7ca927d8988dbf173f1b8d4854c34f7ef4e00a
suripoori <suraj.srinivasa@gmail.com>
2016-01-31 14:33:54 -0500

    Added AJAX for NYTimes articles about city

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -12,7 +12,30 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-
+    var street = $('#street').val();
+    var city = $('#city').val();
+    var address = street + ", " + city;
+    
+    $greeting.text("So you want to live at " + address + "?");
+    var streetViewUrl = "https://maps.googleapis.com/maps/api/streetview?size=600x300&location=";
+    var completeUrl = streetViewUrl + address;
+    
+    $body.append('<img class="bgimg" src="' + completeUrl + '">');
+    //console.log(street);
+    //console.log(city);
+    //console.log(secrets["article-search-api"]);
+    $.getJSON("http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + city + "&sort=newest&api-key=" + secrets["article-search-api"], 
+              function (data) {
+            console.log(data);
+            $.each( data.response.docs, function(index) {
+                console.log(data.response.docs[index].snippet);
+                $("#nytimes-articles").append( "<li class='article'><a href='" + data.response.docs.web_url + "'>" + 
+                                              data.response.docs[index].headline.main + "</a>" + 
+                                              "<p>" + data.response.docs[index].snippet + "</p></li>" );
+                
+            });
+    });
+    
     return false;
 };
 
